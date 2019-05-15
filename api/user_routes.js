76994fc20e064a8112244ce88d2df6256c227545
Hein Crebolder <hc.crebolder@student.avans.nl>
2019-05-15 22:07:55 +0200

    testhelper

--- api/user_routes.js
+++ api/user_routes.js
@@ -3,38 +3,37 @@ var routes = express.Router();
 var db = require('../config/db');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var validator = require("email-validator");
 
 
-routes.post('/user/login', function (req, res) {
-      // var user = {
-      // EmailAddress: req.body.EmailAddress,
-      // Password: req.body.Password
-      // }
-      // let sql = 'SELECT * FROM user WHERE EmailAddress = "' + user.EmailAddress + '"'
-      // db.query(sql, (err, result) => {
-      //     if (err) {
-      //         console.log(err)
-      //     } else {
-      //         var Password = bcrypt.compareSync(req.body.Password, user.Password)
-      //         if (passwordisValid) {
-
-      //             var token = jwt.sign({ id: result.userid }, config.secretkey, {
-      //                 expiresIn: 86400
-      //             })
-      //             res.send('Logged in', { auth: true, token: token }, (200))
-      //             console.log('User logged in')
-      //         }
-      //         if (!passwordisValid) {
-      //             res.send('Password does not match', (401))
-      //             console.log(user)
-      //             console.log(result)
-      //             console.log(passwordisValid)
-      //         }
-
-      //     }
-      // })
+routes.post('/users/login', function(req, res) {
+      let user = {
+            EmailAddress: req.body.EmailAddress,
+          Password: req.body.Password
+      }
+      let sql = 'SELECT password FROM user WHERE EmailAddress = "' + user.EmailAddress + '"'
+      db.query(sql, (err, result) => {
+          if (err) {
+              console.log(err)
+          } else {
+              var passwordisValid = bcrypt.compareSync(req.body.password, result[0].password)
+              if (passwordisValid) {
+  
+                  var token = jwt.sign({user}, config.secretkey, {
+                      expiresIn: 86400
+                  })
+                  res.status(200).send({ auth: true, token: token });
+                 
+              }
+              if (!passwordisValid) {
+                  res.status(401).send({Message: 'Password does not match'})
+              
+              }
+  
+          }
+      })
 })
-routes.post('/user', function  (req, res) {     
+routes.post('/users', function  (req, res) {     
       var user = {
       EmailAddress: req.body.EmailAddress,
       Password: req.body.Password,
@@ -47,15 +46,19 @@ routes.post('/user', function  (req, res) {
       DataOfBirth: req.body.DataOfBirth
 
       }
-      let sql = 'INSERT INTO user(EmailAddress, Password, PhoneNumber, FirstName, LastName, City, StreetAddress, PostalCode, DataOfBirth) VALUES ("' + user.EmailAddress + '", "' + user.Password + '", "' + user.PhoneNumber + '", "' + user.FirstName + '", "' + user.LastName + '", "' + user.City + '", "' + user.StreetAddress + '", "'+ user.DataOfBirth + '", "' + user.PostalCode + '")'
-            db.query(sql, (err, result) => {
-            if (err) {
-                  res.send(err)
-            console.log('Error: ' + err)
-            }else {
-                  res.send(result, 'User created', (200))
-      }
-  })
+      if( validator.validate( EmailAddress)){
+            let sql = 'INSERT INTO user(EmailAddress, Password, PhoneNumber, FirstName, LastName, City, StreetAddress, PostalCode, DataOfBirth) VALUES ("' + user.EmailAddress + '", "' + user.Password + '", "' + user.PhoneNumber + '", "' + user.FirstName + '", "' + user.LastName + '", "' + user.City + '", "' + user.StreetAddress + '", "'+ user.DataOfBirth + '", "' + user.PostalCode + '")'
+                    db.query(sql, (err, result) => {
+                    if (err) {
+                        res.send(err)
+                    console.log('Error: ' + err)
+                    }else {
+                          res.send(result, 'User created', (200))
+                    }
+                    })
+        }else {
+            console.log( 'not an email')
+        }
 })
 
 
