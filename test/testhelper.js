863cb524bd631c236876f0a21813ddb58130e936
Hein Crebolder <hc.crebolder@student.avans.nl>
2019-05-15 22:09:36 +0200

    test helper

---
+++ test/testhelper.js
@@ -0,0 +1,35 @@
+const sql = require('mysql')
+const app = require('../server')
+//Create connection
+const db = sql.createConnection({
+    host: 'localhost',
+    user: 'rental',
+    password: 'localpassword',
+    database: 'rental'
+});
+
+beforeEach((done) => {
+    let usersdelete = 'DELETE FROM users'
+    db.query(usersdelete, (err, result) => {
+        if (err) throw err
+        else {
+
+            let appartmentsdelete = 'DELETE FROM appartments'
+            db.query(appartmentsdelete, (err, result) => {
+                if (err) throw err
+                else {
+
+                    let reservationsdelete = 'DELETE FROM reservations'
+                    db.query(reservationsdelete, (err, result) => {
+                        if (err) throw err
+                        else {
+
+                            done()
+                        }
+                    })
+                }
+            })
+
+        }
+    })
+})
